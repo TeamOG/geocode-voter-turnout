85ff28c2ede3dd6e45a29ca986a2d0bfcbc522eb
Blaze Fugina <blaze.fugina@gmail.com>
2018-03-24 14:20:51 -0500

    More styling.

--- src/home.js
+++ src/home.js
@@ -6,9 +6,12 @@ class Home extends React.Component {
     render() {
         return (
             <div className="Home">
-            <TopBar />
-                <h1>Home</h1>
-                <img class="splashphoto"  src={splashPhoto} alt="splash Photo" />
+                <TopBar />
+                <h1>Welcome to the O.G. Voter Turnout App</h1>
+                <img class="splashphoto" src={splashPhoto} alt="splash Photo" />
+                <div className="homeWelcome">
+                    <p>This application was created as part of the Hennepin County Geo:Code 2018 hackathon. Click on the Map and Reports pages to check out detailed voter information data for the state of Minnesota in the past four statewide elections since 2010.</p>
+                </div>
             </div>
         )
     }
