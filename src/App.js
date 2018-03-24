ae5787237eab65f30d23efa926f7c9be57b8051a
Blaze Fugina <blaze.fugina@gmail.com>
2018-03-24 11:43:12 -0500

    Router is starting to implement.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
+
+//This is start of our application. All of the routes will extend from here.
 class App extends Component {
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Switch>
+      <Route exact path="/" component={Home} />
+        </Switch>
       </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Input extends React.Component {
+    render() {
+        return (
+         <Div className="Home">
+         </Div>   
+        )
+    }
+}
