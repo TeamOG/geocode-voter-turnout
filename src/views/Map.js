00ed765dc7f29ec242aca396188a7f78bdf31b13
Grant <strgraw@gmail.com>
2018-03-24 19:52:04 -0500

    Reports added, mapping tested and failed, JSON loaded directly into data, created better reporting

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import TopBar from '../components/TopBar';
+import MapChoropleth from 'react-d3-map-choropleth';
 
 class Map extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    renderMap() {
+    }
+
     render() {
         return (
             <div className="Map">
                 <TopBar />
-                <h1>Maps</h1>
+                <div className="container">
+                    <div className="row">
+                    </div>
+                    <div className="row">
+                        <h1>Maps</h1>
+                    </div>
+                    <div className="row">
+                        {this.renderMap()}
+                    </div>
+                </div>
             </div>
         )
     }
--- src/views/Report.js
+++ src/views/Report.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import TopBar from '../components/TopBar';
+import 'react-table/react-table.css'
+import ReactTable from 'react-table';
+import {countyCountsCols, countyCountsData} from '../data/OG_CountySubdivision_counts';
 
 class Report extends React.Component {
+    constructor(props) {
+        super(props);
+
+        const countyCountsCols = [
+            { Header: 'State', accessor: 'st' },
+            { Header: 'County', accessor: 'cnty', Cell: props => <a href="#" onClick={this.retrieveData.bind(this, props.value)}>{props.value}</a>},
+            { Header: 'Subdivision', accessor: 'sub_division' },
+            { Header: 'Eligible Voters', accessor: 'elgible_voters' },
+            { Header: 'Registered Voters', accessor: 'registered_voters' },
+            { Header: 'Number of Votes', accessor: 'number_of_votes' },
+            { Header: 'Elgible Not Voted', accessor: 'eligible_not_voted' },
+            { Header: 'Registered Not Voted', accessor: 'registered_not_voted' },
+            { Header: 'Year', accessor: 'Year' },
+        ];
+
+        this.state = { countyCountsCols, countyCountsData, selected: '' }
+    }
+
+
+
+    retrieveData(val, e) {
+        e.preventDefault();
+        console.log(this.state.selected);
+        this.setState({selected: val})
+    }
+
+    renderReactTable() {
+
+
+        console.log(this.state.countyCountsCols);
+        return <ReactTable
+                    columns={this.state.countyCountsCols}
+                    data={this.state.countyCountsData}
+                    defaultPageSize={10}
+                    filterable={true}
+                    defaultFilterMethod={(filter, row, column) => {
+                        const id = filter.pivotId || filter.id
+                        return row[id] !== undefined ? String(row[id]).toLowerCase().startsWith(filter.value.toLowerCase()) : true
+                    }}
+                />
+    }
+
     render() {
         return (
             <div className="Report">
                 <TopBar />
-                <h1>Reports</h1>
+                <div className="container">
+                    <div className="row">
+                    </div>
+                    <div className="row">
+                        <h1>Reports</h1>
+                    </div>
+                    <div className="row">
+
+                        {this.renderReactTable()}
+                    </div>
+                </div>
             </div>
         )
     }
