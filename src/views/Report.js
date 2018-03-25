0283829c35cd322871470555e09b48b15a13f199
Grant <strgraw@gmail.com>
2018-03-24 21:24:13 -0500

    Updates to report, added another dataset table

--- src/views/Report.js
+++ src/views/Report.js
@@ -2,49 +2,72 @@ import React from 'react';
 import TopBar from '../components/TopBar';
 import 'react-table/react-table.css'
 import ReactTable from 'react-table';
-import {countyCountsCols, countyCountsData} from '../data/OG_CountySubdivision_counts';
+import { countyCountsData } from '../data/OG_CountySubdivision_counts';
+import { countyAgeData } from '../data/OG_CountySubdivision_by_Age';
 
 class Report extends React.Component {
     constructor(props) {
         super(props);
 
-        const countyCountsCols = [
-            { Header: 'State', accessor: 'st' },
-            { Header: 'County', accessor: 'cnty', Cell: props => <a href="#" onClick={this.retrieveData.bind(this, props.value)}>{props.value}</a>},
-            { Header: 'Subdivision', accessor: 'sub_division' },
-            { Header: 'Eligible Voters', accessor: 'elgible_voters' },
-            { Header: 'Registered Voters', accessor: 'registered_voters' },
-            { Header: 'Number of Votes', accessor: 'number_of_votes' },
-            { Header: 'Elgible Not Voted', accessor: 'eligible_not_voted' },
-            { Header: 'Registered Not Voted', accessor: 'registered_not_voted' },
-            { Header: 'Year', accessor: 'Year' },
-        ];
+        const countyCounts = {
+            columns: [{ Header: 'State', accessor: 'st' },
+                { Header: 'County', accessor: 'cnty', Cell: props => <a href="#" onClick={this.retrieveData.bind(this, props.value)}>{props.value}</a>},
+                { Header: 'Subdivision', accessor: 'sub_division' },
+                { Header: 'Eligible Voters', accessor: 'eligible_voters' },
+                { Header: 'Registered Voters', accessor: 'registered_voters' },
+                { Header: 'Number of Votes', accessor: 'number_of_votes' },
+                { Header: 'Eligible Not Voted', accessor: 'eligible_not_voted' },
+                { Header: 'Registered Not Voted', accessor: 'registered_not_voted' },
+                { Header: 'Year', accessor: 'Year' }],
+            data: countyCountsData,
+            friendly_name: 'County/Subdivision Vote Counts',
+            slug: 'vote-counts'
+        };
 
-        this.state = { countyCountsCols, countyCountsData, selected: '' }
+        const countyAge = {
+            columns: [{Header: 'State', accessor: 'st'},
+                {Header: 'County', accessor: 'cnty', Cell: props => <a href="#" onClick={this.retrieveData.bind(this, props.value)}>{props.value}</a>},
+                {Header: 'Subdivision', accessor: 'sub_division'},
+                {Header: 'Total Population', accessor: 'tot_pop'},
+                {Header: '0-15', accessor: 'between0_to_15_pop'},
+                {Header: '15-25', accessor: 'between15_to_25_pop'},
+                {Header: '25-45', accessor: 'between25_45_pop'},
+                {Header: '45-59', accessor: 'between45_59_pop'},
+                {Header: '60-85', accessor: 'between60_85_pop'},
+                {Header: '85+', accessor: 'above85_pop'}],
+            data: countyAgeData,
+            friendly_name: 'County/Subdivision Age Totals',
+            slug: 'age-totals'
+        };
+
+        this.state = { data: {countyCounts: countyCounts, countyAge: countyAge}, selected: '', selected_report: '' }
     }
-
-
 
     retrieveData(val, e) {
         e.preventDefault();
-        console.log(this.state.selected);
         this.setState({selected: val})
     }
 
     renderReactTable() {
-
-
         console.log(this.state.countyCountsCols);
-        return <ReactTable
-                    columns={this.state.countyCountsCols}
-                    data={this.state.countyCountsData}
-                    defaultPageSize={10}
-                    filterable={true}
-                    defaultFilterMethod={(filter, row, column) => {
-                        const id = filter.pivotId || filter.id
-                        return row[id] !== undefined ? String(row[id]).toLowerCase().startsWith(filter.value.toLowerCase()) : true
-                    }}
-                />
+        if (this.state.selected == '' && this.state.selected_report == '') {
+            return <p>Click a report.</p>
+        } else {
+            return <ReactTable
+                columns={this.state.data[this.state.selected_report].columns}
+                data={this.state.data[this.state.selected_report].data}
+                defaultPageSize={10}
+                filterable={true}
+                defaultFilterMethod={(filter, row, column) => {
+                    const id = filter.pivotId || filter.id
+                    return row[id] !== undefined ? String(row[id]).toLowerCase().startsWith(filter.value.toLowerCase()) : true
+                }}
+            />
+        }
+    }
+
+    handleReport(report) {
+        this.setState({selected_report: report});
     }
 
     render() {
@@ -53,12 +76,17 @@ class Report extends React.Component {
                 <TopBar />
                 <div className="container">
                     <div className="row">
+                        <div className="col-sm-12">
+                            <h1>Reports</h1>
+                        </div>
                     </div>
                     <div className="row">
-                        <h1>Reports</h1>
+                        <div className="col-sm-12">
+                            <button type="button" className="btn btn-info mr-2" onClick={this.handleReport.bind(this, 'countyCounts')}>Vote Counts</button>
+                            <button type="button" className="btn btn-info mr-2" onClick={this.handleReport.bind(this, 'countyAge')}>Age Counts</button>
+                        </div>
                     </div>
-                    <div className="row">
-
+                    <div className="row mt-4">
                         {this.renderReactTable()}
                     </div>
                 </div>
